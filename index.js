30dbfe72906c4d19c7e99ce0c609df7d89c3de5c
Niti9 <nnegi8298@gmail.com>
2025-09-18 15:27:45 +0530

    Step1.(b) Building the Graph -> Added AI Agent (Langchain-LangGraph) -> Now AI Agent taking question and shows into human Readable Form

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import readLine from "node:readline/promises";
+
+/**
+ * 1.Define  node function
+ * 2. Build the graph
+ * 3. Compile and invoke the graph
+ */
+
+function callModel(state) {
+  //call the LLM using APIS
+
+  console.log("Calling LLM....");
+  return state;
+}
+
+/** Build the Graph */
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent")
+  .addEdge("agent", "__end__");
+
+/** Compile the Graph */
+const app = workflow.compile();
 
 async function main() {
   // Created Interface for input and output of chat
@@ -12,6 +35,11 @@ async function main() {
     console.log("You said: ", userInput);
 
     if (userInput === "bye") break;
+
+    const finalState = await app.invoke({
+      messages: [{ role: "user", content: userInput }],
+    });
+    console.log("Final->> ", finalState);
   }
 
   rl.close();
--- readme.md
+++ readme.md
@@ -14,3 +14,13 @@ bun run index.js
 
 <!-- We will use 'readLine (built-in Node.js module)
  To do chat in terminal -->
+
+# Adding Ai Agents (LangChain-LangGraph)
+
+# Install Langchain-langraph
+
+<!-- website for reference  also it will deprecated soon
+https://langchain-ai.github.io/langgraphjs/tutorials/quickstart/?ajs_aid=65df6784-3281-42c9-8ce2-aaf943e22201
+ -->
+
+bun add @langchain/core @langchain/langgraph
